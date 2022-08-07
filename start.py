01c879b3bcbc4818f6b8d1eae1b0b1d06aff18af
alexha1234 <76671695+alexha1234@users.noreply.github.com>
2022-08-07 18:08:52 -0400

    Update start.py

--- start.py
+++ start.py
@@ -2,4 +2,19 @@ from yelpapi import YelpAPI
 import argparse
 from pprint import pprint
 
-GET https://api.yelp.com/v3/businesses/matches
+argparser = argparse.ArgumentParser(description='Example Yelp queries using yelpapi')
+argparser.add_argument('api_key', type=str, help='Yelp Fusion API Key')
+args = argparser.parse_args()
+
+with YelpAPI(args.api_key) as yelp_api:
+    """
+        Example search by location text and term. 
+        
+        Search API: https://www.yelp.com/developers/documentation/v3/business_search
+    """
+    print('***** 5 best rated ice cream places in Austin, TX *****\n{}\n'.format("yelp_api.search_query(term='ice cream', "
+                                                                                 "location='austin, tx', sort_by='rating', "
+                                                                                 "limit=5)"))
+    response = yelp_api.search_query(term='ice cream', location='austin, tx', sort_by='rating', limit=5)
+    pprint(response)
+    print('\n-------------------------------------------------------------------------\n')

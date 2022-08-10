9f4c9a5821b371f6fb4ab608ab3cba081c328cee
alexha1234 <76671695+alexha1234@users.noreply.github.com>
2022-08-09 20:46:18 -0400

    Update start.py

--- start.py
+++ start.py
@@ -16,13 +16,10 @@ load_dotenv()
 
 api_key = os.getenv("api_key", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
 
-
-#print(api_key)
-
 city = input("Please input your zip code: ")
 cuisine = input('Please input your cuisine: ')
 with YelpAPI(api_key) as yelp_api:
-    search_results = yelp_api.search_query(location = city, term = cuisine, sort_by= 'rating', limit = 5, radius=2000)
+    search_results = yelp_api.search_query(location = zip, term = cuisine, sort_by= 'rating', limit = 5, radius=2000)
 
 #pprint(search_results)
 
